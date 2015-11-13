e77338473ae063e626b15f824fa32e300672cff2
Naman Mittal <namanmittal003@gmail.com>
2015-11-12 21:28:09 -0500

    Define Post method

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ from os import curdir, sep
 import socket
 
 PORT_NUMBER = 8008
+clients_list = list()
 
 class myHandler(BaseHTTPRequestHandler):
         def do_GET(self):
@@ -25,7 +26,10 @@ class myHandler(BaseHTTPRequestHandler):
                         return
                 except IOError:
                                 self.send_error(404, " File not found %s " % self.path)
-
+	def do_POST(self):
+		'''
+		Define here for POST. Parsing of fields
+		'''
 try:
 	server = HTTPServer(('',8008), myHandler)
 	print "Server Started on port ", PORT_NUMBER
