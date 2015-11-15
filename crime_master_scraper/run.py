1346da7dcbf4292aa7bea37943e0421f587dfb2a
Kavana <kavana.anand.9@gmail.com>
2015-11-15 16:56:58 -0500

    tables are added in result html page using dummy data

--- crime_master_scraper/run.py
+++ crime_master_scraper/run.py
@@ -12,10 +12,23 @@ def form():
 def submit():
     page=request.form['webpage']
     print page
-    command = "scrapy crawl crime_master -a start_url="+page+" -o links.csv -t csv"
-    print command
-    os.system(command)
-    return render_template('result.html', page=page)
+
+    # Uncomment the following to run scrapy
+
+    # command = "scrapy crawl crime_master -a start_url="+page+" -o links.csv -t csv"
+    # print command
+    # os.system(command)
+
+    headers = ['A','B','C']
+    numbers = ['3','2','4']
+    headersPages = {	'headers':headers,
+    					'numbers':numbers
+    				}
+    missingpagesList = [	{'headerName':'A','pages':['example.com/a','example.com/b','wassup.com/hello']},
+    						{'headerName':'B','pages':['example.com/a','wassup.com/hi']},
+    						{'headerName':'C','pages':['example.com/a','example.com/b','wassup.com/hello','wassup.com/hi']}
+    					]
+    return render_template('result.html', page=page, headersPages=headersPages, missingpagesList=missingpagesList)
 
 if __name__ == "__main__":
     app.run()
